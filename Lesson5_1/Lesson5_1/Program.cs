fc42cb0e83102e89dbfd3e9ed056289435446cd0
Eugene <gitdelatrue@gmail.com>
2021-02-06 01:55:45 +0500

    Add Lesson 5

---
+++ Lesson5_1/Lesson5_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.	Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+
+            string myFile = "Mytxt.txt";
+            File.WriteAllText(myFile, "Создание текстового файла через VS");
+        }
+    }
+}
