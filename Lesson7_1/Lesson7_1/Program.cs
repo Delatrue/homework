3be2f08e2bba27fa2f3dbefa0207de7ca64b1402
Eugene <gitdelatrue@gmail.com>
2021-02-11 02:01:02 +0500

    Add Lesson 8, Lesson 7, Lesson 6

---
+++ Lesson7_1/Lesson7_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Определить, является ли введённое пользователем число чётным.
+
+            Console.WriteLine("Четное или нечетное? Введите число: ");
+            var num = Convert.ToDouble(Console.ReadLine());
+
+            var result = num % 2;
+            if (result == 0)
+            {
+                Console.WriteLine("Число четное!");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
